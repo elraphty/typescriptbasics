22fa7c5ea198ef95d228e54aac89ef94e4e934c3
elraphty <elraphtyofficial@gmail.com>
2019-02-11 16:35:01 +0100

    basic types

--- types.js
+++ types.js
@@ -1,1 +1,7 @@
-console.log('hello');
+var myString;
+var myNum;
+var myBool;
+myString = 'hello from ts i love u';
+myNum = 2;
+myBool = false;
+console.log(myString, myNum, myBool);
--- types.ts
+++ types.ts
@@ -1,1 +1,9 @@
-console.log('hello');
+var myString: string;
+var myNum: number;
+var myBool: boolean;
+
+myString = 'hello from ts i love u';
+myNum = 2;
+myBool = false;
+
+console.log(myString, myNum, myBool);
