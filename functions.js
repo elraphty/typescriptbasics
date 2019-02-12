d00c33edbb8fe78c0d15c8e860412ef42dea05bb
elraphty <elraphtyofficial@gmail.com>
2019-02-12 08:56:48 +0100

    more basic functions

--- functions.js
+++ functions.js
@@ -1,4 +1,10 @@
 function getSum(num1, num2) {
     return num1 + num2;
 }
-console.log(getSum(1, 2));
+console.log(getSum(1, 3));
+function getName(firstName, lastName) {
+    if (lastName === undefined)
+        return firstName;
+    return firstName + '' + lastName;
+}
+console.log(getName('Raphael'));
--- functions.ts
+++ functions.ts
@@ -2,4 +2,11 @@ function getSum(num1: number, num2: number):number {
   return num1 + num2;
 }
 
-console.log(getSum(1, 2));
+console.log(getSum(1, 3));
+
+function getName(firstName: string, lastName?: string):string {
+  if(lastName === undefined) return firstName;
+  return firstName + '' + lastName;
+}
+
+console.log(getName('Raphael'));
