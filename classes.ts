cc6903e32e9e39c2f9e8c3bbcc5842e054ca755f
elraphty <elraphtyofficial@gmail.com>
2019-02-12 09:41:05 +0100

    added inheritance

--- classes.ts
+++ classes.ts
@@ -14,8 +14,29 @@ class User {
   register() {
     console.log(this.name + ' is now registered');
   }
+
+  payInvoice() {
+    console.log(this.name + ' paid Invoice');
+  }
 }
 
-let john = new User('Raphael', 'elraphtyofficial@gmail.com', 23);
+// let john = new User('Raphael', 'elraphtyofficial@gmail.com', 23);
 
-console.log(john.register());
+// console.log(john.register());
+
+class Member extends User {
+  id: number;
+
+  constructor(id: number, name: string, email: string, age: number) {
+    super(name, email, age);
+    this.id = id;
+  }
+
+  payInvoice() {
+    super.payInvoice();
+  }
+}
+
+let member = new Member(1, 'Raphael', 'elraphtyofficial@gmail.com', 23);
+
+console.log(member.payInvoice());
